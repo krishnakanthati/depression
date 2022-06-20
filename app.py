b94d0bde9ea1204c0df89a3545e67142757fbd8d
Krishna Kant Hati <krishnakant08@users.noreply.github.com>
2022-06-20 21:49:46 +0530

    quiz

--- app.py
+++ app.py
@@ -89,6 +89,10 @@ def get_data():
 def home():
     return render_template('index.html')
 
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
